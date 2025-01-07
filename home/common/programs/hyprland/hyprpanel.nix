371fda20a224e8f22a6361fe9fb2fc469743a2b0
Miguel Palhas <mpalhas@gmail.com>
2025-01-07 17:53:15 +0000

    wip

--- home/common/programs/hyprland/hyprpanel.nix
+++ home/common/programs/hyprland/hyprpanel.nix
@@ -5,5 +5,49 @@
 }:
 
 {
+  programs.hyprpanel = {
+    enable = true;
 
+    systemd.enable = true;
+    overwrite.enable = true;
+
+    layout = {
+      "bar.layouts" = {
+        "0" = {
+          "left" = [
+            "dashboard"
+            "workspaces"
+            "windowtitle"
+          ];
+          "middle" = [ "media" ];
+          "right" = [
+            "volume"
+            "network"
+            "bluetooth"
+            "systray"
+            "clock"
+            "notifications"
+          ];
+        };
+        "1" = {
+          "left" = [ ];
+          "middle" = [ ];
+          "right" = [ ];
+        };
+        "2" = {
+          "left" = [ ];
+          "middle" = [ ];
+          "right" = [ ];
+        };
+      };
+    };
+
+    settings = {
+      theme.bar.transparent = true;
+      theme.font = {
+        name = "Fira Code Nerd Font";
+        size = "14px";
+      };
+    };
+  };
 }
--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -9,7 +9,6 @@
 }:
 {
   imports = [
-    inputs.hyprpanel.homeManagerModules.hyprpanel
     inputs.nix-colors.homeManagerModule
     inputs.nixvim.homeManagerModules.nixvim
     catppuccin.homeManagerModules.catppuccin
@@ -58,51 +57,5 @@
   home = {
     username = lib.mkDefault "naps62";
     stateVersion = lib.mkDefault "24.05";
-  };
-
-  programs.hyprpanel = {
-    enable = true;
-    #
-    #   systemd.enable = true;
-    #   overwrite.enable = true;
-    #
-    #   layout = {
-    #     "bar.layouts" = {
-    #       "0" = {
-    #         "left" = [
-    #           "dashboard"
-    #           "workspaces"
-    #           "windowtitle"
-    #         ];
-    #         "middle" = [ "media" ];
-    #         "right" = [
-    #           "volume"
-    #           "network"
-    #           "bluetooth"
-    #           "systray"
-    #           "clock"
-    #           "notifications"
-    #         ];
-    #       };
-    #       "1" = {
-    #         "left" = [ ];
-    #         "middle" = [ ];
-    #         "right" = [ ];
-    #       };
-    #       "2" = {
-    #         "left" = [ ];
-    #         "middle" = [ ];
-    #         "right" = [ ];
-    #       };
-    #     };
-    #   };
-    #
-    #   settings = {
-    #     theme.bar.transparent = true;
-    #     theme.font = {
-    #       name = "Fira Code Nerd Font";
-    #       size = "14px";
-    #     };
-    #   };
   };
 }
