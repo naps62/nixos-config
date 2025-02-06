cb34bbf46746e5566bbee81992f31d504d8e22b5
Miguel Palhas <mpalhas@gmail.com>
2025-02-06 09:08:16 +0000

    wip

--- home/arrakis/default.nix
+++ home/arrakis/default.nix
@@ -27,7 +27,7 @@
 
   wayland.windowManager.hyprland.settings = {
     workspace = [
-      "1, monitor:DP-1"
+      "1, monitor:DP-3"
       "2, monitor:eDP-1"
       "3, monitor:eDP-1"
       "4, monitor:eDP-1"
