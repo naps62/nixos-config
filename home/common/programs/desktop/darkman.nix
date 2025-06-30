dcbd73a9cff96758ef2a478fe319e07bd97af49d
Miguel Palhas <mpalhas@gmail.com>
2025-06-30 11:24:31 +0100

    fix darkman

--- home/common/programs/desktop/darkman.nix
+++ home/common/programs/desktop/darkman.nix
@@ -8,17 +8,17 @@
     enable = true;
     darkModeScripts = {
       gtk-theme = ''
-        ${pkgs.dconf}/bin/dconf write\
+        ${pkgs.dconf}/bin/dconf write \
         /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-        ${pkgs.dconf}/bin/dconf write\
+        ${pkgs.dconf}/bin/dconf write \
         /org/gnome/desktop/interface/gtk-theme "'Orchis-Dark-Compact'"
       '';
     };
     lightModeScripts = {
       gtk-theme = ''
-        ${pkgs.dconf}/bin/dconf write\
+        ${pkgs.dconf}/bin/dconf write \
         /org/gnome/desktop/interface/color-scheme "'prefer-light'"
-        ${pkgs.dconf}/bin/dconf write\
+        ${pkgs.dconf}/bin/dconf write \
         /org/gnome/desktop/interface/gtk-theme "'Orchis-Compact'"
       '';
     };
