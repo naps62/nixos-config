3e3ad71e2743117cf5379cf51d7128c87a559e05
Miguel Palhas <mpalhas@gmail.com>
2025-07-03 18:34:31 +0100

    hypridle

--- home/common/programs/hyprland/default.nix
+++ home/common/programs/hyprland/default.nix
@@ -17,7 +17,6 @@ in
     hyprshot
     playerctl
     hyprsunset
-    hypridle
     hyprlock
   ];
 
@@ -212,6 +211,29 @@ in
     settings = {
       ipc = "on";
       splash = false;
+    };
+  };
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
+      listener = [
+        {
+          timeout = 900;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+
     };
   };
 }
