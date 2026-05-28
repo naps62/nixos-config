0f1c9235e9e2299746bca0eb07ae8f939e735e3d
Miguel Palhas <mpalhas@gmail.com>
2026-05-28 15:24:48 +0100

    wip

--- home/common/programs/dev.nix
+++ home/common/programs/dev.nix
@@ -51,5 +51,6 @@
     nss.tools
 
     kicad
+    tea
   ];
 }
--- home/common/programs/ssh.nix
+++ home/common/programs/ssh.nix
@@ -5,11 +5,13 @@
     matchBlocks = {
       "yolo" = {
         hostname = "10.7.10.2";
-        remoteForwards = [{
-          bind.port = 9222;
-          host.address = "localhost";
-          host.port = 9222;
-        }];
+        remoteForwards = [
+          {
+            bind.port = 9222;
+            host.address = "localhost";
+            host.port = 9222;
+          }
+        ];
       };
     };
   };
